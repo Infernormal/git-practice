9515feebbab9ea93221d43ab680e50fae8e8be84
Lilly <waterlilly169@GMAIL.COM>
2021-09-17 14:40:09 -0700

    <Lilly>

--- function_b.py
+++ function_b.py
@@ -7,8 +7,18 @@ def silly_sum():
         the user enters 0, or
         the sum reaches or exceeds 1000
     """
-    pass
+    total = 0
+    while True:
+        valid_input = input_int()
+        if valid_input == 0:
+            break
+        elif total < 1000:
+            total += valid_input
+        else:
+            break
+    return total
 
+   
 
 if __name__ == "__main__":
     print(f"Answer = {silly_sum()}")
